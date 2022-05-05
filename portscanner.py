e754ffe98f2cd493f0b0e5f7a5ac03efba1ad9e8
seanp97 <sean.pelser97@gmail.com>
2022-05-05 16:13:34 +0100

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -8,32 +8,22 @@ class PortScanner:
 
         if len(sys.argv) > 0:
             self.hostip = sys.argv[1]
-
-            self.timeout = 0.15
-            self.portlimit = 65535
-
-            if sys.argv[2] != "":
-                self.timeout = float(sys.argv[2])
-
-            if sys.argv[3] != "":
-                self.portlimit = int(sys.argv[3])
-
-            self.ScanNetwork(self.hostip, self.timeout, self.portlimit)
+            self.ScanNetwork(self.hostip)
         else:
             print("Enter valid IP address")
             sys.exit()
 
 
     
-    def ScanNetwork(self, ipaddr, timeout, portlimit):
+    def ScanNetwork(self, ipaddr, timeout = 0.1, portlimit = 65535):
         self.startTime = time.time()
 
         self.t_IP = gethostbyname(ipaddr)
         print(f"Starting scan on host: {self.t_IP}")
         
-        for i in range(1, portlimit):
+        for i in range(1, 65535):
             self.s = socket(AF_INET, SOCK_STREAM)
-            self.s.settimeout(timeout)
+            self.s.settimeout(0.15)
             self.conn = self.s.connect_ex((self.t_IP, i))
             if(self.conn == 0):
                 print (f"Port {i}: OPEN")
